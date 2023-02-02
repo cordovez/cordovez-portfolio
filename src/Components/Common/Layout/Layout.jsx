972fb483afa6343a9ba6bd7dbaf43a0050eb98bb
JC CorMan <cordovez@el-cordovez.com>
2023-02-02 18:48:06 +0100

    made index the portfolio component

--- src/Components/Common/Layout/Layout.jsx
+++ src/Components/Common/Layout/Layout.jsx
@@ -1,9 +1,11 @@
 import { useState } from "react";
-
+import { useSelectedLayoutSegment } from "next/navigation";
 import { Hero } from "@/Components/Hero/Hero";
 import Footer from "../Footer/Footer";
 
 export default function Layout({ children }) {
+  // useSelectedLayoutSegment();
+
   const [night, setNight] = useState(true);
 
   return (
